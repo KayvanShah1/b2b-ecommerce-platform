35d055bf24565666e055ac722c04019ae0e59ad1
Kayvan Shah <32059-Kayvan-shah@users.noreply.git.toptal.com>
2026-03-29 02:40:00 +0530

    reset/delete buckets

---
+++ packages/b2b_ec_sources/src/b2b_ec_sources/nuke_buckets.py
@@ -0,0 +1,10 @@
+from b2b_ec_utils.storage import storage
+
+
+def nuke(bucket_name: str = None):
+    print(f"Deleting {bucket_name}...")
+    storage.fs.rm(bucket_name, recursive=True)
+
+
+if __name__ == "__main__":
+    nuke("b2b-ec-marketing")
