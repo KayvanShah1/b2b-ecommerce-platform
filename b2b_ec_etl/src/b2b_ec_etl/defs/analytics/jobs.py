baa9c2ade866e8f3c7ed48203020d50e6ca22f36
Kayvan Shah <32059-Kayvan-shah@users.noreply.git.toptal.com>
2026-03-30 21:08:59 +0530

    rename for better readability

--- b2b_ec_etl/src/b2b_ec_etl/defs/analytics/jobs.py
+++ b2b_ec_etl/src/b2b_ec_etl/defs/analytics/jobs.py
@@ -2,10 +2,10 @@ from dagster import AssetSelection, define_asset_job
 
 from b2b_ec_etl.defs.analytics.asset import dbt_models
 
-job_build_dbt_models = define_asset_job(
-    name="job_build_dbt_models",
+build_analytics_models = define_asset_job(
+    name="build_analytics_models",
     selection=AssetSelection.assets(dbt_models),
     description="Build analytics models with dbt.",
 )
 
-analytics_jobs = [job_build_dbt_models]
+analytics_jobs = [build_analytics_models]
--- b2b_ec_etl/src/b2b_ec_etl/defs/analytics/resources.py
+++ b2b_ec_etl/src/b2b_ec_etl/defs/analytics/resources.py
@@ -1,7 +1,7 @@
 from b2b_ec_utils import settings
 from dagster_dbt import DbtCliResource, DbtProject
 
-dbt_project = DbtProject(project_dir=settings.dbt_project_dir, profiles_dir=settings.dbt_profiles_dir)
+dbt_project = DbtProject(project_dir=settings.dbt.project_dir, profiles_dir=settings.dbt.profiles_dir)
 dbt_project.prepare_if_dev()
 dbt_resource = DbtCliResource(project_dir=dbt_project)
 
