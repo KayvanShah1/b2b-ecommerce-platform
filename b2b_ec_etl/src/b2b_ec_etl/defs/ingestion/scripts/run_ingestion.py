c24e970b8a88a97b1a3efad4beae15f15c14f228
Kayvan Shah <32059-Kayvan-shah@users.noreply.git.toptal.com>
2026-04-02 00:09:13 +0530

    feat: implement timed run for full ingestion ETL process

--- b2b_ec_etl/src/b2b_ec_etl/defs/ingestion/scripts/run_ingestion.py
+++ b2b_ec_etl/src/b2b_ec_etl/defs/ingestion/scripts/run_ingestion.py
@@ -1,12 +1,14 @@
 from b2b_ec_etl.defs.ingestion.scripts.load_runner import run_load_only
 from b2b_ec_etl.defs.ingestion.scripts.raw_capture_runner import run_process_only, run_raw_capture
+from b2b_ec_utils.timer import timed_run
 
 
-def run():
+@timed_run(name="Full Ingestion ETL Run")
+def run_ingestion_etl():
     run_raw_capture()
     run_process_only()
     run_load_only()
 
 
 if __name__ == "__main__":
-    run()
+    run_ingestion_etl()
